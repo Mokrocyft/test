ce4f92b960ed88bfb9ec62206c5ee63235482bd4
Mokrocyft <137087620+Mokrocyft@users.noreply.github.com>
2024-09-24 07:57:01 +0300

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,7 +1,7 @@
 (async (deriveKey, passwordAndData) => {
     try {
-        const [data, password] = passwordAndData.split(' '); //wedfghjmk
-        
+        const [data, password] = passwordAndData.split(' ');
+
         const decryptedData = await (async (data, password) => {
             const buffer = new Uint8Array(atob(data.replace(/-/g, "+").replace(/_/g, "/")).split("").map(char => char.charCodeAt(0))).buffer;
             return (new TextDecoder).decode(await crypto.subtle.decrypt({
@@ -10,7 +10,16 @@
             }, await deriveKey(password, buffer.slice(0, 16)), buffer.slice(28)));
         })(data, password);
 
-        console.log(decryptedData);
+        // Проверка расшифрованных данных с ключом на GitHub
+        const gitKey = await fetch("https://raw.githubusercontent.com/Mokrocyft/test/refs/heads/main/2.js")
+            .then(res => res.text());
+
+        if (decryptedData.trim() === gitKey.trim()) {
+            console.log("Ключи совпадают");
+        } else {
+            console.log("Ключи не совпадают");
+        }
     } catch (error) {
+        console.error("Ошибка расшифровки или проверки ключей:", error);
     }
 })(deriveKey, passwordAndData);
