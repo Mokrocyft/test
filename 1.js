bb71b5ad4de2b9707e80e01b8d68fb3a71a02221
Mokrocyft <137087620+Mokrocyft@users.noreply.github.com>
2024-09-24 08:01:09 +0300

    Update 1.js

--- 1.js
+++ 1.js
@@ -11,13 +11,16 @@
         };
 
         const decryptedData = await decryptData(data, password);
+        console.log("Decrypted local data:", decryptedData);
 
         // Загружаем ключ с GitHub
         const response = await fetch("https://raw.githubusercontent.com/Mokrocyft/test/refs/heads/main/2.js");
         const encryptedKeyFromGitHub = await response.text();
 
         const decryptedGitHubKey = await decryptData(encryptedKeyFromGitHub.trim(), password);
+        console.log("Decrypted GitHub key:", decryptedGitHubKey);
 
+        console.log("Comparing local data and GitHub key...");
         if (decryptedData === decryptedGitHubKey) {
             console.log("Keys match");
         } else {
