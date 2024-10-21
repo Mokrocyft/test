bb01973dbe5c6c30a4837c7f90b315f00d922333
Mokrocyft <137087620+Mokrocyft@users.noreply.github.com>
2024-10-21 23:07:49 +0300

    Update 1.js

--- 1.js
+++ 1.js
@@ -9,11 +9,18 @@
         }, await deriveKey(password, buffer.slice(0, 16)), buffer.slice(28)));
     };
 
+    // Загружаем ключ из JSON на GitHub
+    const response = await fetch("https://raw.githubusercontent.com/Mokrocyft/test/refs/heads/main/key.json");
+    if (!response.ok) return console.error(`Ошибка: ${response.statusText}`);
+
+    const dataFromGithub = await response.json();
+    const githubKey = dataFromGithub.key; // Получаем ключ из JSON
+
+    // Расшифровываем введенный ключ
     const decryptedData = await decryptData(data);
     console.log("Decrypted data:", decryptedData);
 
-    const response = await fetch("https://raw.githubusercontent.com/Mokrocyft/test/refs/heads/main/2.js");
-    const encryptedKeyFromGitHub = await response.text();
-    const decryptedGitHubKey = await decryptData(encryptedKeyFromGitHub);
-    console.log("Decrypted GitHub key:", decryptedGitHubKey);
+    // Сравниваем расшифрованный ключ с ключом из GitHub
+    console.log(`Ключ с GitHub: "${githubKey}"`);
+    console.log(decryptedData === githubKey ? "Ключи совпадают!" : "Ключи не совпадают.");
 })(deriveKey, passwordAndData);
